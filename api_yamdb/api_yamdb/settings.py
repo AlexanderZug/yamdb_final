62f0b139fcce4d7539ea810c2e65d1ae9724b5b1
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-11-09 10:08:20 +0500

    commit 30

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='default')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default='default')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['*', ])
 
 AUTH_USER_MODEL = 'users.User'
 
